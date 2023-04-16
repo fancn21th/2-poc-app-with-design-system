b1cf1ed3c5b09852208c793ee9ebabba44e5e072
fancn21th <fancn21th@gmail.com>
2023-04-16 17:53:10 +0800

    feat(design-system): auto docs

--- packages/design-system/components/Button/Button.stories.tsx
+++ packages/design-system/components/Button/Button.stories.tsx
@@ -1,5 +1,6 @@
 // Button.stories.ts|tsx
 import React from 'react';
+
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Button from './Button';
@@ -7,14 +8,18 @@ import Button from './Button';
 const meta = {
   title: 'Design System/Components/Button',
   component: Button,
+  tags: ['autodocs'],
 } satisfies Meta<typeof Button>;
 
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
-  args: {
-    primary: true,
-    label: 'Button',
+export const Primary: Story = (args) => <Button {...args} />;
+
+Primary.args = {
+  label: 'Button',
+  onClick: () => {
+    return null;
   },
 };
--- packages/design-system/components/Button/Button.tsx
+++ packages/design-system/components/Button/Button.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const StyledButton = styled.button``;
@@ -8,8 +9,24 @@ type ButtonProps = {
   onClick: () => void;
 };
 
-const Button = ({ label, onClick }: ButtonProps) => {
+const Button = ({ label = 'Button', onClick }: ButtonProps) => {
   return <StyledButton onClick={onClick}>{label}</StyledButton>;
 };
+
+// Button.defaultProps = {
+//   label: 'Button',
+//   onClick: () => {
+//     return null;
+//   },
+// };
+
+// /**
+//  * be aware the propTypes has to be compatible with the type of props defined in ts
+//  **/
+
+// Button.propTypes = {
+//   label: PropTypes.string.isRequired,
+//   onClick: PropTypes.func.isRequired,
+// };
 
 export default Button;
